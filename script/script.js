40aadcec972f195168b81ca95395ec0fe633fda7
leonel-carvajal <liosoft777@gmail.com>
2020-11-11 18:43:55 -0600

    Modal terminada y programada

--- script/script.js
+++ script/script.js
@@ -13,6 +13,73 @@ const getCharacters = async () => {
     console.log(error)
   }
 }
+const f = ()=>{
+  const secciones = document.getElementsByTagName('section')
+  const modal = document.getElementById('modal')
+  const modal_content = document.getElementById('modal__content')
+  const img = document.getElementById('img1')
+  const fragment2 = document.createDocumentFragment()
+  const modalName = document.getElementById('modal_name')
+  const modalTitulo = document.getElementById('titulos')
+  const modalCara = document.getElementById('modal_cara')
+  const modalApa = document.getElementById('modal_apa')
+  const modalBio = document.getElementById('modal_bio')
+  const modalWork = document.getElementById('modal_work')
+  const modalConexion = document.getElementById('modal_conexion')
+  //--------------------------------------------
+  for (const cards of secciones) {
+    // const img = document.createElement('img')
+    cards.addEventListener('click', () => {
+      modal.classList.add('show')
+      fetch(`https://akabab.github.io/superhero-api/api/id/${cards.childNodes[0].textContent}.json`)
+        .then(res => res.json())
+        .then(data => {
+          modalName.textContent = data.name
+          img.src = data.images.sm
+          modalCara.children[0].textContent = 'Características'
+          modalCara.children[1].textContent = `Inteligencia: ${data.powerstats.intelligence}`
+          modalCara.children[2].textContent = `Fuerza: ${data.powerstats.strength}`
+          modalCara.children[3].textContent = `Velocidad: ${data.powerstats.speed}`
+          modalCara.children[4].textContent = `Durabilidad: ${data.powerstats.durability}`
+          modalCara.children[5].textContent = `Poder: ${data.powerstats.power}`
+          modalCara.children[6].textContent = `Combate: ${data.powerstats.combat}`
+          modalCara.children[7].textContent = `Editora: ${data.biography.publisher}`
+
+          modalApa.children[0].textContent = 'Apariencia'
+          modalApa.children[1].textContent = `Genero: ${data.appearance.gender}`
+          modalApa.children[2].textContent = `Raza: ${data.appearance.race}`
+          modalApa.children[3].textContent = `Altura: ${data.appearance.height[1]}`
+          modalApa.children[4].textContent = `Peso: ${data.appearance.weight[1]}`
+          modalApa.children[5].textContent = `Ojos: ${data.appearance.eyeColor}`
+          modalApa.children[6].textContent = `Cabello: ${data.appearance.hairColor}`
+
+          modalBio.children[0].textContent = 'Biografía'
+          modalBio.children[1].textContent = `Nombre completo: ${data.biography.fullname}`
+          modalBio.children[2].textContent = `Alter Ego: ${data.biography.alterEgos}`
+          modalBio.children[3].textContent = `Alias: ${data.biography.aliases}`
+          modalBio.children[4].textContent = `Lugar Nacimiento: ${data.biography.placeOfBirth}`
+          modalBio.children[5].textContent = `Primera Aparición: ${data.biography.firstAppearance}`
+          modalBio.children[6].textContent = `Editora: ${data.biography.publisher}`
+          modalBio.children[7].textContent = `Bando: ${data.biography.alignment}`
+
+          modalWork.children[0].textContent = 'Trabajo'
+          modalWork.children[1].textContent = `Ocupacion: ${data.work.occupation}`
+          modalWork.children[2].textContent = `Base:${data.work.base}`
+
+          modalConexion.children[0].textContent = 'Equipos'
+          modalConexion.children[1].textContent = `Afiliación: ${data.connections.groupAffiliation}`
+          modalConexion.children[2].textContent = `Relativos: ${data.connections.relatives}`
+        })
+    })
+    modalTitulo.appendChild(modalName)
+    modalTitulo.appendChild(img)
+    fragment2.appendChild(modalTitulo)
+  }
+  modal_content.appendChild(fragment2)
+  modal.addEventListener('click', () => {
+    modal.classList.remove('show')
+  })
+}
 const pintar = async (rango = 0) => {
   const data = await getCharacters()
   const fragment = document.createDocumentFragment();
@@ -58,52 +125,7 @@ const pintar = async (rango = 0) => {
   }
   contenedor.appendChild(fragment);
 //---------------------------------
-  const secciones = document.getElementsByTagName('section')
-  const modal = document.getElementById('modal')
-  const modal_content = document.getElementById('modal__content')
-  const img = document.getElementById('img1')
-  const fragment2 = document.createDocumentFragment()
- const modalName = document.getElementById('modal_name')
- const modalTitulo = document.getElementById('titulos')
- const modalCara =document.getElementById('modal_cara')
- const modalApa = document.getElementById('modal_apa')
- //--------------------------------------------
-  for (const cards of secciones) {
-    // const img = document.createElement('img')
-    cards.addEventListener('click', () => {
-      modal.classList.add('show')
-      fetch(`https://akabab.github.io/superhero-api/api/id/${cards.childNodes[0].textContent}.json`)
-        .then(res => res.json())
-        .then(data => {
-          modalName.textContent = data.name
-          img.src = data.images.sm
-          modalCara.children[0].textContent = 'Características'
-          modalCara.children[1].textContent = `Inteligencia: ${data.powerstats.intelligence}`
-          modalCara.children[2].textContent = `Fuerza: ${data.powerstats.strength}`
-          modalCara.children[3].textContent = `Velocidad: ${data.powerstats.speed}`
-          modalCara.children[4].textContent = `Durabilidad: ${data.powerstats.durability}`
-          modalCara.children[5].textContent = `Poder: ${data.powerstats.power}`
-          modalCara.children[6].textContent = `Combate: ${data.powerstats.combat}`
-          modalCara.children[7].textContent = `Editora: ${data.biography.publisher}`
-
-          modalApa.children[0].textContent = 'Apariencia'
-          modalApa.children[1].textContent = `Genero: ${data.appearance.gender}`
-          modalApa.children[2].textContent = `Raza: ${data.appearance.race}`
-          modalApa.children[3].textContent = `Altura: ${data.appearance.height[1]}`
-          modalApa.children[4].textContent = `Peso: ${data.appearance.weight[1]}`
-          modalApa.children[5].textContent = `Ojos: ${data.appearance.eyeColor}`
-          modalApa.children[6].textContent = `Cabello: ${data.appearance.hairColor}`
-          
-        })
-      })
-      modalTitulo.appendChild(modalName)
-      modalTitulo.appendChild(img)
-      fragment2.appendChild(modalTitulo)
-    }
-    modal_content.appendChild(fragment2)
-  modal.addEventListener('click', () => {
-    modal.classList.remove('show')
-  })
+ f()
 
 }
 const filterName = async (data) => {
@@ -111,7 +133,6 @@ const filterName = async (data) => {
   const fragment = document.createDocumentFragment()
   if (data.length >= 3) {
     for (let i = 0; i < datos.length; i++) {
-      //const element = datos[i];
       if (datos[i].name === data) {
         contenedor.innerHTML = ''
         const card = document.createElement("section");
@@ -158,6 +179,7 @@ const filterName = async (data) => {
       }
       contenedor.appendChild(fragment)
     }
+    f()
   } else {
     contenedor.innerHTML = ''
     paintCharacters(25)
@@ -175,12 +197,14 @@ const filterName = async (data) => {
 
 }
 buscar.addEventListener('keyup', (e) => {
+  
   if (buscar.value != '') {
     filterName(e.target.value)
   }
+
 })
 const paintCharacters = (rango) => {
-  rango = 24
+  rango = 25
   pintar(rango)
 
 }
