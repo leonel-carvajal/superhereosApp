1cf3f559b8f4499a13151844063346c6404df81a
leonel-carvajal <liosoft777@gmail.com>
2020-12-06 10:48:17 -0600

    v0.2

--- script/team.js
+++ script/team.js
@@ -1,5 +1,6 @@
 let arrayTeam = []
 let count = 0
+const barra = document.getElementById('barra')
 const getAllTeam = async () => {
   const reponse = await fetch('https://akabab.github.io/superhero-api/api/all.json')
   const data = await reponse.json()
@@ -15,4 +16,18 @@ const getTeam = async () => {
     arrayTeam.push(teams.connections.groupAffiliation)
   }
 }
+const imagenes = () => {
+  const imagenes = ['dccomics.jpg', 'dcMarvel.jpg', 'fondo.jpg', 'fondo3.jpg', 'marvel.jpg', 'hero1.jpg',
+    'hero2.jpg', 'hero3.jpg', 'maevel2.jpg', 'hero3.jpg', 'hero4.jpg', 'hero5.jpg']
+  let cont = 0
+  let ultima = imagenes.length - 1
+  setInterval(() => {
+    barra.style.backgroundImage = `url(/img/${imagenes[cont]})`
+    cont++
+    if (cont > ultima || cont == ultima) {
+      cont = 0
+    }
+  }, 10000)
+}
+imagenes()
 window.addEventListener('DOMContentLoaded', getTeam, false)
